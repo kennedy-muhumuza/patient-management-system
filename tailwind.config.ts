a5e6c1ff6c7feb76955d5025bf85ce2e8532d98f
kennedy-muhumuza <muhumuzakennedy17@gmail.com>
2024-10-03 23:41:37 +0300

    clean up tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,3 @@
-// import type { Config } from "tailwindcss";
-
-// const config: Config = {
-//   content: [
-//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-//   ],
-//   theme: {
-//     extend: {
-//       colors: {
-//         background: "var(--background)",
-//         foreground: "var(--foreground)",
-//       },
-//     },
-//   },
-//   plugins: [],
-// };
-// export default config;
-
 import type { Config } from "tailwindcss";
 
 const { fontFamily } = require("tailwindcss/defaultTheme");
