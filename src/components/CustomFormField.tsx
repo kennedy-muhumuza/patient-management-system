bd84750cc4216ee9f2b8c6523117c28dfa30be50
kennedy-muhumuza <muhumuzakennedy17@gmail.com>
2024-10-27 14:26:28 +0300

    set default country

--- src/components/CustomFormField.tsx
+++ src/components/CustomFormField.tsx
@@ -80,7 +80,7 @@ const RenderInput = ({ field, props }: { field: any; props: CustomProps }) => {
       return (
         <FormControl>
           <PhoneInput
-            defaultCountry="US"
+            defaultCountry="UG"
             placeholder={props.placeholder}
             international
             withCountryCallingCode
